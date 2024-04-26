b6cbc9149636a202452618fe0534fac06778458c
ramsivaji <ramsivaji0@gmail.com>
2024-04-26 20:46:03 +0530

    first commit

---
+++ sample1.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    a=x+y
+    return a
+def sub (x,y):
+    a=x-y
+    return a
+
+c=add(10,20)
+print(c)
+
+d=sub(50,20)
+print(d)
+
+
---
+++ sample2.py
@@ -0,0 +1,7 @@
+a=int(input("enter any  number"))
+b=int(input("enter any  number"))
+c=a+b
+print(c)
+x=int(input("enter any number"))
+for i in range(1,4):
+    print("*"*4)
